f722bc2088e4f25b5b7279ed29431a3b5525d77a
ParkSeokgyu <gongsilmoa@gmail.com>
2024-01-23 01:45:56 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
-/* header_search */
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
 
 
 /* header_badges */
@@ -164,8 +148,3 @@ spyEls.forEach(spyEl => {
   .setClassToggle(spyEl, 'show')
   .addTo(new ScrollMagic.Controller());
 });
-
-
-// footer copyright Year
-const thisYear = document.querySelector('footer .this-year');
-thisYear.textContent = new Date().getFullYear();
